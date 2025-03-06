33da3c370967541931fe0af7663ed0aa9fc13f02
henryak25 <henryadityakosasi@gmail.com>
2025-03-06 22:43:45 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -14,10 +14,50 @@ public class Payment {
     Map<String, String> paymentData;
 
     Payment(String id, String method, Map<String, String> paymentData) {
-
+        this.checkPaymentData(paymentData);
+        this.paymentData = paymentData;
+        this.id = id;
+        this.method = method;
+        this.setStatus("REJECTED");
     }
 
     Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.checkPaymentData(paymentData);
+        this.paymentData = paymentData;
+        this.setStatus(status);
+        this.id = id;
+        this.method = method;
+    }
 
+    public void setStatus(String status) {
+        if (status.equals("SUCCESS") || status.equals("REJECTED")) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void checkPaymentData(Map<String, String> paymentData){
+        if(paymentData == null){
+            throw new IllegalArgumentException();
+        } else if (!paymentData.containsKey("voucherCode") && !paymentData.containsKey("bankName") && !paymentData.containsKey("referenceCode")) {
+            throw new IllegalArgumentException();
+        } else {
+            if(paymentData.get("voucherCode").isEmpty() || paymentData.get("voucherCode").length()!=16){
+                throw new IllegalArgumentException();
+            }
+            if(!paymentData.get("voucherCode").startsWith("ESHOP")){
+                throw new IllegalArgumentException();
+            }
+            if(paymentData.get("voucherCode").isEmpty()){
+                throw new IllegalArgumentException();
+            }
+            if(paymentData.get("bankName").isEmpty()){
+                throw new IllegalArgumentException();
+            }
+            if(paymentData.get("referenceCode").isEmpty()){
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
