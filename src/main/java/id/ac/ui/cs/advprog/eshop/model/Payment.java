c760bc3c848150c0b27ae5dcfc8c0e1327af8232
henryak25 <henryadityakosasi@gmail.com>
2025-03-06 22:54:50 +0700

    [REFACTOR] Apply PaymentStatus enum check to Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import java.util.Map;
 
 @Getter
@@ -18,7 +19,7 @@ public class Payment {
         this.paymentData = paymentData;
         this.id = id;
         this.method = method;
-        this.setStatus("REJECTED");
+        this.status = PaymentStatus.REJECTED.getValue();
     }
 
     Payment(String id, String method, String status, Map<String, String> paymentData) {
@@ -30,7 +31,7 @@ public class Payment {
     }
 
     public void setStatus(String status) {
-        if (status.equals("SUCCESS") || status.equals("REJECTED")) {
+        if (PaymentStatus.contains(status)) {
             this.status = status;
         } else {
             throw new IllegalArgumentException();
