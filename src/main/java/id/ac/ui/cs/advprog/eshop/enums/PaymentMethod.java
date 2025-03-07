4bce35d80ff9aae81788f93087adad728c360450
henryak25 <henryadityakosasi@gmail.com>
2025-03-07 20:40:26 +0700

    [REFACTOR] Add PaymentMethod

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.eshop.enums;
+import lombok.Getter;
+
+@Getter
+public enum PaymentMethod {
+
+    VOUCHER("Voucher Code"),
+    BANK("Bank Transfer");
+
+    private final String value;
+    private PaymentMethod(String value) { this.value = value; }
+
+    public static boolean contains(String param) {
+        for (PaymentMethod status : PaymentMethod.values()) {
+            if (status.value.equals(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
