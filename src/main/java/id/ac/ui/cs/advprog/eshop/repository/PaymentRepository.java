a7e983dab5629fa453ee36f2b8c91bdca710c5b0
henryak25 <henryadityakosasi@gmail.com>
2025-03-07 11:49:30 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -10,17 +10,35 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList();
 
     public Payment save(Payment payment) {
-        return null;
+        for (Payment paymentData: paymentData) {
+            if (paymentData.getId().equals(payment.getId())){
+                throw new IllegalStateException();
+            }
+        }
+        paymentData.add(payment);
+        return payment;
     }
     public Payment findById(String id) {
+        for (Payment payment : paymentData){
+            if (payment.getId().equals(id)){
+                return payment;
+            }
+        }
         return null;
     }
 
     public Payment update(Payment payment) {
-        return null;
+        String id = payment.getId();
+        Payment paymentInRepository = this.findById(id);
+        if (paymentInRepository == null) {
+            return null;
+        }
+        paymentData.remove(paymentInRepository);
+        paymentData.add(payment);
+        return payment;
     }
 
     public List<Payment> getAllPayment() {
-        return null;
+        return paymentData;
     }
 }
